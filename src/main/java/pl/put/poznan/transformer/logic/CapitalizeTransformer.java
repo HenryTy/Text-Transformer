91e170cc1788fff2d2066ff982a098343ee1cdaa
Lt04 <jakub.k.borowiak@student.put.poznan.pl>
2019-11-09 17:39:39 +0100

    Poprawa capitalize

--- src/main/java/pl/put/poznan/transformer/logic/CapitalizeTransformer.java
+++ src/main/java/pl/put/poznan/transformer/logic/CapitalizeTransformer.java
@@ -3,7 +3,13 @@ package pl.put.poznan.transformer.logic;
 public class CapitalizeTransformer implements TextTransformer{
 
     public String transform(String text){
-        String capitalized = text.substring(0, 1).toUpperCase() + text.substring(1);
-        return capitalized;
+        String[] words = text.split(" ");
+        StringBuilder resultBuilder = new StringBuilder();
+        for(int i = 0; i < words.length; i++) {
+            String capitalized = words[i].substring(0, 1).toUpperCase() + words[i].substring(1);
+            resultBuilder.append(capitalized);
+            resultBuilder.append(" ");
+        }
+        return resultBuilder.toString();
     }
 }
