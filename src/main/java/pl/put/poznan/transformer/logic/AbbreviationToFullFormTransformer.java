2e28746f91af480df6064bbb592b8ed680aa1a89
jfraszczak <jakub.fraszczak37@gmail.com>
2019-11-12 09:47:53 +0100

    Rozwijanie skrótów

---
+++ src/main/java/pl/put/poznan/transformer/logic/AbbreviationToFullFormTransformer.java
@@ -0,0 +1,33 @@
+package pl.put.poznan.transformer.logic;
+
+public class AbbreviationToFullFormTransformer implements TextTransformer {
+
+    public String transform(String text){
+        String[] words = text.split(" ");
+        StringBuilder resultBuilder = new StringBuilder();
+
+        String[] abbreviations = {"prof.", "dr", "np.", "itd."};
+        String[] full_forms = {"profesor", "doktor", "na przykład", "i tym podobne"};
+        boolean IsAbbreviation;
+        for(int i = 0; i < words.length; i++) {
+            IsAbbreviation = false;
+            for(int j = 0; j < abbreviations.length; j++){
+                if(words[i].equalsIgnoreCase(abbreviations[j])){
+                    IsAbbreviation = true;
+                    String FullForm = words[i].charAt(0) + full_forms[j].substring(1);
+                    resultBuilder.append(FullForm);
+                }
+            }
+            if(!IsAbbreviation){
+                resultBuilder.append(words[i]);
+            }
+            if(i < words.length - 1){
+                resultBuilder.append(" ");
+            }
+        }
+        if(words[words.length - 1].equalsIgnoreCase("itd.")){
+            resultBuilder.append(".");
+        }
+        return resultBuilder.toString();
+    }
+}
