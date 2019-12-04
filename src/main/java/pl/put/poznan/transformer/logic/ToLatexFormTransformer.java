47b9704ff4c124579f32b64a4833746b9526e3bb
Lt04 <48619839+Lt04@users.noreply.github.com>
2019-12-04 16:46:58 +0100

    Add files via upload
    zamiana na latexową formę

---
+++ src/main/java/pl/put/poznan/transformer/logic/ToLatexFormTransformer.java
@@ -0,0 +1,21 @@
+package pl.put.poznan.transformer.logic;
+
+public class ToLatexFormTransformer extends TextTransformerDecorator{
+    public ToLatexFormTransformer(TextTransformer textTransformer) {
+        super(textTransformer);
+    }
+    public String transform(String text){
+        char[] text_before = text.toCharArray();
+        StringBuilder text_after = new StringBuilder();
+        for(int i = 0;i < text_before.length; i++){
+            if(text_before[i] == '$' || text_before[i] == '&'){
+                text_after.append('\\');
+                text_after.append(text_before[i]);
+            }
+            else{
+                text_after.append(text_before[i]);
+            }
+        }
+        return text_after.toString();
+    }
+}
